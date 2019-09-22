4589fca2a667fcfb26420e4205cbc7f661993d82
yoann01 <ygranier@gmail.com>
2019-09-23 01:51:10 +0200

    more formats

--- init.py
+++ init.py
@@ -14,4 +14,8 @@ sys.path.append(py)
 
 # custom formats
 nuke.addFormat('320 240 1.0 RFX_320p')
-nuke.knobDefault('Root.format', 'RFX_320p')
+#nuke.knobDefault('Root.format', 'RFX_320p')
+nuke.addFormat('3072 3072 square_3K')
+nuke.addFormat('4096 4096 square_4K')
+nuke.addFormat('3424 2202 AlexaMini_OpenGate')
+nuke.addFormat('3200 1800 Amira_3.2K')
